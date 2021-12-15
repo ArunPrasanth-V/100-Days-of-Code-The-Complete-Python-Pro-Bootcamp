eef09ae98343d4c14a99353df0a326d3e2cffb97
ArunPrasanth-V <88626504+ArunPrasanth-V@users.noreply.github.com>
2021-12-15 19:51:23 +0530

    Create day1FinalProject.py

---
+++ Day1/day1FinalProject.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator ! ")
+city =input("What name of the city you Grow in up \n")
+pet=input("Your pett Animal Name !\n")
+print("So, your band name is "+city+" "+pet)
