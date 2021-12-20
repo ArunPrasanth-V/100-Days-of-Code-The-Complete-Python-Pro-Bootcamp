5a2b60a665317555ea66738586e73ead7423a894
ArunPrasanth-V <88626504+ArunPrasanth-V@users.noreply.github.com>
2021-12-20 22:16:08 +0530

    Create livingYearMonthsDayWeeks.py

---
+++ Day2/livingYearMonthsDayWeeks.py
@@ -0,0 +1,6 @@
+currentAge=int(input("Enter Your Current Age : "))
+remainYear=90-currentAge
+reaminDay=remainYear*365
+remainweek=remainYear*52
+remainMonth=remainYear*12
+print(f"Wow! your are going to alive for {remainYear} year , {remainMonth} Month , {remainweek} weeks , {reaminDay} Days" )
