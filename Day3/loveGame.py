79e2e22fb84e9a176ccd70dae2b73824acdd4b24
ArunPrasanth-V <88626504+ArunPrasanth-V@users.noreply.github.com>
2021-12-20 22:23:37 +0530

    Update loveGame.py

--- Day3/loveGame.py
+++ Day3/loveGame.py
@@ -4,3 +4,17 @@ name1=name1.lower()
 name2=input("Enter your Lover name !")
 name2=name2.lower()
 name1+=name2
+truecount+=name1.count("t");
+truecount+=name1.count("r");
+truecount+=name1.count("u");
+truecount+=name1.count("e");
+
+truecount=truecount*10
+
+truecount+=name1.count("l");
+truecount+=name1.count("o");
+truecount+=name1.count("v");
+truecount+=name1.count("e");
+
+print(f"your love score is {truecount}")
+
