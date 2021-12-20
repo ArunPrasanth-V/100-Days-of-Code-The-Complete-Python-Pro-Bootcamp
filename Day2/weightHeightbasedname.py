a4949a3be7454d7bf2670892fb05acf071df77fd
ArunPrasanth-V <88626504+ArunPrasanth-V@users.noreply.github.com>
2021-12-20 22:18:38 +0530

    Create weightHeightbasedname.py

---
+++ Day2/weightHeightbasedname.py
@@ -0,0 +1,8 @@
+a=float(input("Enter your weight"))
+b=float(input("Enter Your Height"))
+print(int(a/(b*b)))
+
+num=len(input("What is your Name ?"))
+new_num=str(num)
+print("Your Name has ",new_num," character")
+
