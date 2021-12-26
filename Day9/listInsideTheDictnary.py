133fabc2f1faecd2874bfff25eb76cb988db87de
ArunPrasanth-V <88626504+ArunPrasanth-V@users.noreply.github.com>
2021-12-26 23:01:14 +0530

    Create listInsideTheDictnary.py

---
+++ Day9/listInsideTheDictnary.py
@@ -0,0 +1,4 @@
+developers={"Arun":["Java Programmer","Problem Solver","webDevelopment Beginner"],
+         "Vimal":["OpenSourse Enthuc","C++ Programmer","Github Guru"]}
+# nesting Dictionary inside a Dictionary
+countary={"Canada":{"city":["Toronto","Ontario","ottawa"],"visited":0}}
