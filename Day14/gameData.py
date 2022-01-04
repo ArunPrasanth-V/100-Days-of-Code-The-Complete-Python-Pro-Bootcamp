b7ab50c9000f7c02b40f92076a595648c3127344
ArunPrasanth-V <88626504+ArunPrasanth-V@users.noreply.github.com>
2022-01-04 15:15:58 +0530

    Create gameData.py

---
+++ Day14/gameData.py
@@ -0,0 +1,74 @@
+data=[
+     {
+        'name': 'instagram',
+        'Follower_count': 346,
+        'description ':'social media platform',
+        'country': 'united state'
+    },
+     {
+        'name': 'Arun',
+        'Follower_count': 500,
+        'description ': 'Programming platform ',
+        'country': 'Canada'
+    },
+     {
+        'name': 'Vimal',
+        'Follower_count': 500,
+        'descriptaion ':'Programming platform ',
+        'country': 'India'
+    },
+     {
+        'name': 'kishore',
+        'Follower_count': 450,
+        'description ':'social media platform',
+        'country': 'united state'
+    },
+     {
+        'name': 'Nishanth',
+        'Follower_count': 400,
+        'description ':'Gym Guy',
+        'country': 'U.K'
+    },
+     {
+        'name': 'Mithin',
+        'Follower_count': 399,
+        'description ':'programmer',
+        'country': 'Japan'
+    },
+     {
+        'name': 'Aswin',
+        'Follower_count': 356,
+        'description ':'X Media Platform',
+        'country': 'united state'
+    },
+     {
+        'name': 'Giri',
+        'Follower_count': 402,
+        'description ':'Python',
+        'country': 'India'
+    },
+     {
+        'name': 'thulaci',
+        'Follower_count': 410,
+        'description ':'Delta guy',
+        'country': 'Tamil Nadu'
+    },
+     {
+        'name': 'Sanni ',
+        'Follower_count': 344,
+        'description ':'social media platform',
+        'country': 'united state'
+    },
+     {
+        'name': 'Tessa',
+        'Follower_count': 349,
+        'description ':'media platform',
+        'country': 'Australia'
+    },
+     {
+        'name': 'mark',
+        'Follower_count': 200,
+        'description ':'social media ',
+        'country': 'united state'
+    }
+]
