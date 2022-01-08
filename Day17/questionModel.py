122aaabceeab09189c7c9a9cbbd5695030da7667
ArunPrasanth-V <88626504+ArunPrasanth-V@users.noreply.github.com>
2022-01-08 21:42:27 +0530

    Create questionModel.py

---
+++ Day17/questionModel.py
@@ -0,0 +1,12 @@
+question = [
+    {"text": "1+1=2", "answer": True},
+    {"text": "1+2=3", "answer": True},
+    {"text": "1+0=2", "answer": False},
+    {"text": "0+1=2", "answer": False},
+    {"text": "1+1=2", "answer": True},
+    {"text": "0+0=2", "answer": False},
+    {"text": "1+1=2", "answer": True},
+    {"text": "2+2=4", "answer": True},
+    {"text": "3+1=4", "answer": True}
+
+]
