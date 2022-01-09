e5f51b507c1b723439762a5ec1e1b0d35084d243
ArunPrasanth-V <88626504+ArunPrasanth-V@users.noreply.github.com>
2022-01-09 18:50:48 +0530

    Create turtle_sample.py

---
+++ Day18/turtle_sample.py
@@ -0,0 +1,18 @@
+from turtle import Turtle,Screen
+
+def mov_that():
+    timmy_the_turtle.right(90)
+    timmy_the_turtle.forward(100)
+
+timmy_the_turtle=Turtle()
+
+timmy_the_turtle.shape("turtle")
+timmy_the_turtle.pencolor("red")
+
+for i in range(4):
+    mov_that()
+
+
+
+screen=Screen()
+screen.exitonclick()
