f6b8b6fe4a6edad708d92587d64072cb7fbcd007
ArunPrasanth-V <88626504+ArunPrasanth-V@users.noreply.github.com>
2022-01-09 18:49:32 +0530

    Create entryForDeveloper.py

---
+++ Day18/entryForDeveloper.py
@@ -0,0 +1,9 @@
+class Student:
+    def __init__(self,student_name,student_rollNO,student_job,field_of_intrest):
+        self.name=student_name
+        self.roll=student_rollNO
+        self.job=student_job
+        self.intrest=field_of_intrest
+s1=Student("ArunPrasanth","CSA19013","Devops","Java")
+s2=Student("Vimal","CSA19112","Devops","C++")
+s3=Student("kiahore","CSA19045","Developer","Java")
