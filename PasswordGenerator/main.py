f1511837b6054618d140cb8c4e7d3bb1294a7d0f
ArunPrasanth-V <88626504+ArunPrasanth-V@users.noreply.github.com>
2022-02-17 09:38:26 +0530

    Update main.py

--- PasswordGenerator/main.py
+++ PasswordGenerator/main.py
@@ -6,10 +6,6 @@ canvas = Canvas(root, width = 400, height = 480)
 img = PhotoImage(file="lock.png")
 canvas.create_image(20,20, anchor=NW, image=img)
 canvas.grid(row=0,column=1)
-#Label
-
-
-
 
 canvas.pack()
 mainloop()
